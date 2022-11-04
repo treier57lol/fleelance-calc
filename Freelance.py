71d149a96f6429d7820a12d1baa4aed0581ec46f
xn <play.fpl.andwin@gmail.com>
2022-11-04 19:18:18 +0200

    Esimene katse

--- Freelance.py
+++ Freelance.py
@@ -5,3 +5,7 @@ import sqlite3
 connection  = sqlite3.connect("Freelance alfa.db")
 
 cursor = connection.cursor()
+muutuja = input("Sisesta teenus vms: ")
+fetch = cursor.execute(f"""SELECT * FROM HIND
+WHERE teenused = {muutuja}""")
+fetch.fetchone()
