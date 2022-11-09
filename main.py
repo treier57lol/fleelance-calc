bf786568aabe9904c295f0515782186c8d2e08bc
treierxyz <me@treier.xyz>
2022-11-09 22:26:30 +0200

    Parem loop, vigase sisendi käsitlemine

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import sqlite3
-connection  = sqlite3.connect("Freelance alfa.db")
-def valik(muutuja):
-    rahulolu = ""
-    while rahulolu != -1:
-        cursor = connection.cursor()
-        fetch = cursor.execute(f"SELECT hind FROM teenused WHERE teenus = \"{muutuja}\"")
+
+connection = sqlite3.connect("Freelance alfa.db")
+rahulolu = False
+
+def valik():
+    teenus1 = input("Millist teenust soovid müüa? (väljumiseks kirjuta 'välju') ")
+    if teenus1 == "välju":
+        return True
+    cursor = connection.cursor()
+    fetch = cursor.execute(f"SELECT hind FROM teenused WHERE teenus = \"{teenus1}\"")
+    try:
         raha = fetch.fetchone()[0]
-        print("Antud teenuse eest oleks optimaalne küsida", raha, "eurot")
-        rahulolu = input("Kas soovid veel valida? (jah/ei) ")
-        if rahulolu == "jah":
-            muutuja = input("Sisesta siis teenus vms: ")
-        elif rahulolu == "ei":
-            break
-        else:
-            rahulolu = -1
-teenus1 = input("Millist teenust soovid müüa? ")
-esialgne = str(teenus1)
-valik(esialgne)
+    except:
+        print("Sellist teenust ei leitud!")
+        return False
+    print(f"Antud teenuse eest oleks optimaalne küsida {raha} eurot")
+    jätka = input("Kas soovid veel valida? (jah/ei) ")
+    if jätka == "jah":
+        return False
+    elif jätka == "ei":
+        return True
+
+while not rahulolu:
+    rahulolu = valik()
