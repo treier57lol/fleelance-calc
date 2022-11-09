5b448a386d1fe2b8abab757de40b4c84757f8b0b
xn <play.fpl.andwin@gmail.com>
2022-11-09 20:52:47 +0200

    Update main.py
    Peaks okei olema alfaks

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import sqlite3
-
 connection  = sqlite3.connect("Freelance alfa.db")
-
-cursor = connection.cursor()
-muutuja = input("Sisesta teenus vms: ")
-fetch = cursor.execute(f"SELECT hind FROM teenused WHERE teenus = \"{muutuja}\"")
-print(fetch.fetchone()[0])
-
-
+def valik(muutuja):
+    rahulolu = ""
+    while rahulolu != -1:
+        cursor = connection.cursor()
+        fetch = cursor.execute(f"SELECT hind FROM teenused WHERE teenus = \"{muutuja}\"")
+        raha = fetch.fetchone()[0]
+        print("Antud teenuse eest oleks optimaalne küsida", raha, "eurot")
+        rahulolu = input("Kas soovid veel valida? (jah/ei) ")
+        if rahulolu == "jah":
+            muutuja = input("Sisesta siis teenus vms: ")
+        elif rahulolu == "ei":
+            break
+        else:
+            rahulolu = -1
+teenus1 = input("Millist teenust soovid müüa? ")
+esialgne = str(teenus1)
+valik(esialgne)
